237a3336aebb88bf2d1984b85b241299d12bb6ff
Mario Mendoza <menriquemendoza@unicesar.edu.co>
2021-05-11 21:27:29 -0500

    Appoiment class created

---
+++ lib/models/appoiment.dart
@@ -0,0 +1,17 @@
+class Appoiment {
+  int _appoimentId;
+  String _appoimentState;
+  DateTime _dateService;
+  String _description;
+  String _careStaffId;
+  String _patientId;
+
+  Appoiment(
+    this._appoimentId,
+    this._appoimentState,
+    this._dateService,
+    this._description,
+    this._careStaffId,
+    this._patientId,
+  );
+}
